448fbe961c113e3c3704c690e36a4f0785679ca0
Declan Davis <dexthefish@gmail.com>
2022-12-05 15:00:13 -0500

    add partial solution

--- 16-power-digit-sum.js
+++ 16-power-digit-sum.js
@@ -1,7 +1,16 @@
 /*
 
-215 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+2^15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
 
-What is the sum of the digits of the number 21000?
+What is the sum of the digits of the number 2^1000?
 
 */
+
+let n = 2 ** 1000;
+let sum = 0;
+while (n > 1) {
+  sum += n % 10;
+  n = Math.floor(n / 10);
+}
+
+console.log(sum);
