45d2f3dd235264753a67f0094031c3f23047399d
Declan Davis <dexthefish@gmail.com>
2022-12-08 13:57:19 -0500

    add readme

---
+++ 20-factorial-digit-sum.js
@@ -0,0 +1,28 @@
+/*
+n! means n × (n − 1) × ... × 3 × 2 × 1
+
+For example, 10! = 10 × 9 × ... × 3 × 2 × 1 = 3628800,
+and the sum of the digits in the number 10! is 3 + 6 + 2 + 8 + 8 + 0 + 0 = 27.
+
+Find the sum of the digits in the number 100!
+*/
+
+const factorial = (n) => {
+  // return n factorial
+  while (n > 1) {
+    return n * factorial(n - 1);
+  }
+  return 1;
+};
+
+const sumDigits = (n) => {
+  let sum = 0;
+  let num = n;
+  while (num >= 10) {
+    sum += num % 10;
+    num = num / 10;
+  }
+  return sum;
+};
+
+console.log(sumDigits(factorial(100)));
---
+++ README.md
@@ -0,0 +1,3 @@
+The following awesome resource was discovered while doing some research for problem 20.
+
+https://euler.stephan-brumme.com/
