51656f677c184b5f74e2901d2131aa300a2d6f38
Declan Davis <dexthefish@gmail.com>
2022-12-13 09:40:16 -0500

    add problem statements

---
+++ 820-Nth-digit-of-reciprocals.js
@@ -0,0 +1,10 @@
+/*
+
+https://projecteuler.net/problem=820
+
+Let dn(x) be the nth decimal digit of the fractional part of x, or 0 if the fractional part has fewer than n digits.
+
+let S(n) = SUM(k=1 to k=n) dn(1/k).
+
+Find S(10**7).
+*/
