15948b736d027138b21c5f7e1add0d665b7ff735
Declan Davis <dexthefish@gmail.com>
2022-12-12 22:31:21 -0500

    add solution

--- 31-coin-sums.js
+++ 31-coin-sums.js
@@ -7,3 +7,38 @@ It is possible to make £2 in the following way:
 1×£1 + 1×50p + 2×20p + 1×5p + 1×2p + 3×1p
 How many different ways can £2 be made using any number of coins?
 */
+
+const differentWaysToMake = (sum) => {
+  let numberOfWays = 0;
+  for (let ones = 0; ones <= sum; ones++) {
+    for (let twos = 0; 2 * twos <= sum; twos++) {
+      for (let fives = 0; 5 * fives <= sum; fives++) {
+        for (let tens = 0; 10 * tens <= sum; tens++) {
+          for (let twenties = 0; 20 * twenties <= sum; twenties++) {
+            for (let fifties = 0; 50 * fifties <= sum; fifties++) {
+              for (let pounds = 0; 100 * pounds <= sum; pounds++) {
+                for (let twoPounds = 0; 200 * twoPounds <= sum; twoPounds++) {
+                  let total =
+                    ones +
+                    2 * twos +
+                    5 * fives +
+                    10 * tens +
+                    20 * twenties +
+                    50 * fifties +
+                    100 * pounds +
+                    200 * twoPounds;
+                  if (total === sum) {
+                    numberOfWays += 1;
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+  return numberOfWays;
+};
+
+console.log(differentWaysToMake(200));
