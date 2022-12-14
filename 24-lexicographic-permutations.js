2baec4cf9bb2d20e24678fc7e0b34fd4499327fc
Declan Davis <dexthefish@gmail.com>
2022-12-13 20:28:41 -0500

    add efficient solution

--- 24-lexicographic-permutations.js
+++ 24-lexicographic-permutations.js
@@ -5,63 +5,11 @@
 What is the millionth lexicographic permutation of the digits 0, 1, 2, 3, 4, 5, 6, 7, 8 and 9?
 */
 
-const permutations = (s) => {
-  // this function uses too much memory.
-  let permutationsArray = [];
+// --------- SOLUTION ------------
 
-  for (let i = 0; i < 10; i++) {
-    // i represents the index of the first digit to be chosen.
-    // if i=1 then the permutation begins with a 1, for example.
-    for (let j = 0; j < 10; j++) {
-      for (let k = 0; k < 10; k++) {
-        for (let l = 0; l < 10; l++) {
-          for (let m = 0; m < 10; m++) {
-            for (let n = 0; n < 10; n++) {
-              for (let o = 0; o < 10; o++) {
-                for (let p = 0; p < 10; p++) {
-                  for (let q = 0; q < 10; q++) {
-                    for (let r = 0; r < 10; r++) {
-                      for (let t = 0; t < 10; t++) {
-                        let indices = [j, k, l, m, n, o, p, q, r, t];
-                        let distinct = indices.reduce(
-                          (prev, cur) => prev * (prev - cur),
-                          1
-                        );
-                        if (distinct) {
-                          permutationsArray.push(
-                            s[i] +
-                              s[j] +
-                              s[k] +
-                              s[l] +
-                              s[m] +
-                              s[n] +
-                              s[o] +
-                              s[p] +
-                              s[q] +
-                              s[r] +
-                              s[t]
-                          );
-                        }
-                      }
-                    }
-                  }
-                }
-              }
-            }
-          }
-        }
-      }
-    }
-  }
-  return permutationsArray.sort((a, b) => a - b)[999999];
-};
+// There are 9! permutations that begin with the digit 0, and 9! permutations that begin with the digit 1. Together, these make up the first ~700,000 entries in the lexicographical order. The next batch of permutations (those beginning with the digit 2) takes us up to ~1,050,000 and must therefore contain the 1,000,000th entry. We conclude that the answer begins with the digit 2. This number 2 arose because 9! fits into 1,000,000 twice without going over.
 
-// there are 10! total permutations, which is about 3.6 million.
-// they are partitioned evenly by choice of first digit; for example there are 9! permutations beginning with a zero.
-
-// DERIVING THE FIRST DIGIT
-
-// the first digit is 2 because there are 9! permutations that begin with 0 and 9! permutations that begin with 1. These smallest ones make up 9! + 9! = 720,000 (approx) of the total.
+// We then require the ~300,000th entry among those beginning with 2. To find its first (technically second) digit, we ask how many times 8! fits into 300,000 without going over, and so on. The answer to each division question tells us which index to choose from when placing the next digit. The process terminates when there are no more digits to be placed.
 // ------
 
 function factorial(n) {
