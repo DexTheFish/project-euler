da1ed8cefee735cd442f7f725320dba7d886c02e
Declan Davis <dexthefish@gmail.com>
2022-12-16 15:12:02 -0500

    add working solution

--- 16-power-digit-sum.js
+++ 16-power-digit-sum.js
@@ -6,15 +6,37 @@ What is the sum of the digits of the number 2^1000?
 
 */
 
-let n = 2 ** 1000;
-let sum = 0;
-while (n > 1) {
-  sum += n % 10;
-  n = Math.floor(n / 10);
-}
+const sumOfDigitsAttempt = (n) => {
+  // this approach does not work for large n (including n = 1000).
+  // the problem is that precision is lost when dealing with regular numbers larger than 2^53.
+  let number = 2 ** n;
+  let sum = 0;
+  while (number > 1) {
+    // add the rightmost digit, truncate, repeat.
+    sum += number % 10;
+    number = Math.floor(number / 10);
+  }
+  return sum;
+};
 
-console.log(sum);
+const sumOfDigits = (n) => {
+  // the solution uses BigInt https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/BigInt which is able to perform arithmetic of large numbers.
 
-// the problem is that precision is lost when dealing with numbers larger than 2^53
-// the solution is to instead deal with BigInt https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/BigInt
-//
+  // calculate 2 ** n by doubling n times
+  let exponent = 1;
+  let number = BigInt("2");
+  while (exponent < n) {
+    exponent++;
+    number += number;
+  }
+  // separate 2 ** n into an array of single-digit numbers
+  let digitArr = String(number)
+    .split("")
+    .map((x) => Number(x));
+  // add together all the digits
+  let digitSum = digitArr.reduce((prev, cur) => prev + cur, 0);
+  return digitSum;
+};
+
+console.log("------- sum of digits of 2 ** 1000: -----------");
+console.log(sumOfDigits(1000));
