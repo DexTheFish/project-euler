2772b1db126230726c22e19e8d4c4e274fad9a8c
Declan Davis <dexthefish@gmail.com>
2022-12-16 21:59:40 -0500

    add preliminary solution

--- 21-amicable-numbers.js
+++ 21-amicable-numbers.js
@@ -11,6 +11,20 @@ const d = (n) => {
 
 const sumOfAmicableNumbers = (n) => {
   // return the sum of amicable numbers less than n.
+  // to make this function more efficient we should precompute an array
+  // of d(m) for 1 < m < n.
+  console.log("beginning!");
   let sum = 0;
+  for (let i = 1; i < n - 1; i++) {
+    console.log("test1");
+    for (let j = i + 1; j < n; j++) {
+      console.log(i, j);
+      if (d(i) === j && d(j) === i) {
+        sum += i + j;
+        console.log("stepping!", i, j);
+      }
+    }
+  }
   return sum;
 };
+console.log(sumOfAmicableNumbers(10000));
