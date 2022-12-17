a208364cab1d3a74e27de507b6136c36af7067fb
Declan Davis <dexthefish@gmail.com>
2022-12-17 17:27:21 -0500

    add skeleton solution

--- 26-reciprocal-cycles.js
+++ 26-reciprocal-cycles.js
@@ -14,3 +14,25 @@ Where 0.1(6) means 0.166666..., and has a 1-digit recurring cycle. It can be see
 
 Find the value of d < 1000 for which 1/d contains the longest recurring cycle in its decimal fraction part.
 */
+
+// How does one work out a decimal expansion by hand?
+// Applying the division algorithm, we recognize a cycle when a step's remainder is equal to a previous remainder. Let us therefore carry out the division algorithm while keeping track of the remainders and checking for duplicates.
+
+const calculateCycleLength = (n) => {
+  // this should perform the division algorithm on 1/n while collecting data to determine the cycle length of the decimal expansion of 1/n.
+  return 1;
+};
+
+const answer = (n) => {
+  // return the value of d < n for which 1/d contains the longest recurring cycle in its decimal expansion.
+  let cycleLengths = [0, 0];
+  for (let i = 2; i < n; i++) {
+    let cycleLength = calculateCycleLength(i);
+    cycleLengths.push(cycleLength);
+  }
+
+  let maxLength = cycleLengths.reduce((prev, cur) => Math.max(prev, cur));
+  return maxLength;
+};
+
+console.log(answer(1000));
