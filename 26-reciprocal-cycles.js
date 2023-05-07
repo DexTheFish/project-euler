b572dec85b4c884740a8003040dff8b489a1a301
Declan Davis <dexthefish@gmail.com>
2023-05-07 14:56:06 -0400

    initial solution

--- 26-reciprocal-cycles.js
+++ 26-reciprocal-cycles.js
@@ -18,10 +18,89 @@ Find the value of d < 1000 for which 1/d contains the longest recurring cycle in
 // How does one work out a decimal expansion by hand?
 // Applying the division algorithm, we recognize a cycle when a step's remainder is equal to a previous remainder. Let us therefore carry out the division algorithm while keeping track of the remainders and checking for duplicates.
 
-const calculateCycleLength = (n) => {
+const calculateCycleLength = (divisor) => {
   // this should perform the division algorithm on 1/n while collecting data to determine the cycle length of the decimal expansion of 1/n.
-  return 1;
+  let remainders = [];
+  let result = "";
+  let dividend = 1;
+
+  // the algorithm proceeds until we get a remainder of zero (terminating)
+  // or until we get a previously-encountered remainder (repeating)
+  while (new Set(remainders).size === remainders.length && dividend != 0) {
+
+    // move over by some place values until division can be performed
+    while (dividend < divisor) {
+      dividend *= 10;
+      result += '0';
+    }
+
+    // use the whole part of the division to add a digit to the result
+    let quotient = Math.floor(dividend / divisor);
+    result += String(quotient);
+    
+    // get the remainder after division
+    let remainder = dividend % divisor;
+
+    // use the remainder as the new dividend
+    dividend = remainder;
+
+    // keep track of the remainder
+    remainders.push(remainder);
+  }
+  return remainders.length - 1;
 };
+
+let [max, n] = [0, 0];
+for (let i = 2; i < 1000; i++){
+  let cycleLength = calculateCycleLength(i);
+  if (cycleLength > max) {
+    max = cycleLength;
+    n = i; 
+  }
+}
+console.log(max, n);
+
+
+// const calculateCycleLength = (divisor) => {
+//   if (!Number.isInteger(divisor) || divisor <= 0) {
+//     throw new Error('divisor must be a positive integer');
+//   }
+
+//   let remainders = [];
+//   let quotientDigits = "";
+//   let dividend = 1;
+
+//   while (!remainders.includes(dividend) || dividend === 0) {
+//     while (dividend < divisor) {
+//       dividend *= 10;
+//       quotientDigits += "0";
+//     }
+//     let quotient = Math.floor(dividend / divisor);
+//     let remainder = dividend % divisor;
+//     dividend = remainder;
+
+//     quotientDigits += String(quotient);
+//     remainders.push(dividend);
+//   }
+
+//   const firstIndex = remainders.indexOf(dividend);
+//   const secondIndex = remainders.lastIndexOf(dividend);
+//   const cycleLength = secondIndex - firstIndex;
+//   console.log(quotientDigits, remainders, cycleLength)
+//   return {
+//     quotientDigits,
+//     remainders,
+//     cycleLength
+//   };
+// };
+
+
+calculateCycleLength(10);
+
+
+
+
+
 
 const answer = (n) => {
   // return the value of d < n for which 1/d contains the longest recurring cycle in its decimal expansion.
@@ -35,4 +114,4 @@ const answer = (n) => {
   return maxLength;
 };
 
-console.log(answer(1000));
+//console.log(answer(1000));
