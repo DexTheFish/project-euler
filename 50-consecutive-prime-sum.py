0110581a9f5da91e0acbc8478a90954e13cdc34a
Declan Davis <dexthefish@gmail.com>
2023-07-06 13:06:11 -0400

    add solution analyzing consecutive prime sums

--- 50-consecutive-prime-sum.py
+++ 50-consecutive-prime-sum.py
@@ -8,16 +8,17 @@ Which prime, below one-million, can be written as the sum of the most consecutiv
 
 
 def primesArr(n):
-    arr = [True] * n
-    for i in range(2, n):
+    arr = [True] * (n + 5)
+    for i in range(2, n + 1):
         if arr[i]:
-            for j in range(2, n // i):
+            for j in range(2, (n + 1) // i):
                 arr[i * j] = False
     primes = []
     for i in range(2, n):
         if arr[i]:
             primes.append(i)
     return primes
+
 
 # note that the final answer is composed of at least 6 terms
 # therefore an upper bound for `start` is 10**6 / 6, approximately 160 000.
@@ -27,24 +28,35 @@ def primesArr(n):
 # while primes[start] + ... + primes[end] < 10**6, increment the end index. Take note of any step where the sum is a prime, and update prime and count accordingly.
 
 
-limit = 10 ** 3
+limit = 10 ** 6  # one-million
 arr = primesArr(limit)
-arrMax = arr[-1]
+arrMax = arr[-1]  # the largest prime below limit
+arrLength = len(arr)
 start = 0    # starting index
-end = 0    # ending index
-currentSum = 2   # sum(primes[start:end])
-mostTermSum = 2    # candidate for final answer
-mostTerms = 6       # number of summands that form the candidate
+mostTermSum = 0   # candidate for final answer
+mostTerms = 0     # number of summands that form the candidate
+
+while start < arrLength:
+    currentSum = 0
+    end = start
+    while currentSum < limit and end < arrLength:
+        currentSum += arr[end]
+        end += 1
+        if end - start >= mostTerms and currentSum in arr:
+            mostTermSum = currentSum
+            mostTerms = end - start
+    start += 1
+
+print(mostTermSum, mostTerms)
 
 
-increment = 1
-while start < limit / mostTerms:
+# while start < limit / mostTerms:
 
-    if currentSum > arr[-1]:
-        case = 1
-    # case 1: currentSum exceeds 1 million
-        # i. we just incremented 'end' too far. need to increment 'start' once and then start decrementing 'end'
-        # ii. we havent yet decremented 'end' enough, and need to continue.
+#     if currentSum > arr[-1]:
+#         case = 1
+#     # case 1: currentSum exceeds 1 million
+#         # i. we just incremented 'end' too far. need to increment 'start' once and then start decrementing 'end'
+#         # ii. we havent yet decremented 'end' enough, and need to continue.
 
-    # case 2: mostTerms exceeds end - start + 1
-        # i.
+#     # case 2: mostTerms exceeds end - start + 1
+#         # i.
